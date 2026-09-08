08300091800a1a5dde37b8fac727adeb95ebfd75
Kevin Pedrozo <kevin.pedrozo@ccgrupo.com.co>
2026-09-08 12:57:03 -0500

    fix: auto-generación de remember_token y email_verified_at en evento creating del modelo User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Spatie\Permission\Models\Role;
 
@@ -42,6 +43,8 @@ class UserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
         ]);
 
         $user->assignRole($request->role);
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,14 +9,27 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'email_verified_at', 'remember_token'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    protected static function booted(): void
+    {
+        static::creating(function ($user) {
+            if (is_null($user->email_verified_at)) {
+                $user->email_verified_at = now();
+            }
+            if (is_null($user->remember_token)) {
+                $user->remember_token = Str::random(10);
+            }
+        });
+    }
 
     /**
      * Get the attributes that should be cast.
